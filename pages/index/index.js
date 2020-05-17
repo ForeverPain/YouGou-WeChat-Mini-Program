5c126c082cc3c34d134be6bb1418c6d046ca41fb
ForeverPain <foreverpain3@gmail.com>
2020-05-17 20:38:00 +0800

    商品分类区域完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,9 @@ import {
 Page({
   data: {
     //轮播图数组
-    swiperList: []
+    swiperList: [],
+    //导航数组
+    cateList:[] 
 
   },
   //options(Object)
@@ -20,13 +22,25 @@ Page({
     //     })
     //   },
     // });
-    request({
-        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
-      })
-      .then(result => {
-        this.setData({
-          swiperList: result.data.message
-        })
-      })
+   //
+   this.gitSwiperList();
+   this.gitCateList()
   },
+  //获取轮播图数据
+  gitSwiperList(){
+    request({url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    .then(result => {
+      this.setData({
+        swiperList: result.data.message
+      })
+    })
+  },
+  gitCateList(){
+    request({url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+    .then(result => {
+      this.setData({
+        cateList : result.data.message
+      })
+    }) 
+  }
 });
