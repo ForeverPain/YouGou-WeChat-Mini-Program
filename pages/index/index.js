4975e7dcbbb90a541088dc7ed559530071e0a98c
ForeverPain <foreverpain3@gmail.com>
2020-05-17 20:14:44 +0800

    轮播图优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,32 @@
+import {
+  request
+} from "../../request/index.js";
+
 //Page Object
 Page({
   data: {
-   //轮播图数组
-   swiperList:[]
+    //轮播图数组
+    swiperList: []
 
   },
   //options(Object)
-  onLoad: function(options){
-    var reqTask = wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result)=>{ 
-        console.log(result);
+  onLoad: function (options) {
+    // var reqTask = wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result)=>{ 
+    //     console.log(result);
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   },
+    // });
+    request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+      })
+      .then(result => {
         this.setData({
-          swiperList:result.data.message
+          swiperList: result.data.message
         })
-      },
-    });
+      })
   },
 });
